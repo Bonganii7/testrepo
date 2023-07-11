f97fea0fd7da75d2b6e24ebb68b97d1da6c2d27c
Bonganii7 <138070035+Bonganii7@users.noreply.github.com>
2023-07-11 22:29:13 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,38 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+#def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+ #   print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+#if __name__ == '__main__':
+ #   print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+#def check_guests(guest_list, guest):
+
+ # for guest_Key, guest_value in guest_list.items():
+  #    if guest_Key == guest:
+   #       result = guest_value
+
+  #return result
+
+#car = {"Ford": 123, "Volks":222, "Toyota":333}
+
+#print(car.keys())
+
+
+#Return the value for the given key
+
+
+#guest_list = {"Adam": 3, "Camila": 3, "David": 5, "Jamal": 3, "Charley": 2, "Titus": 1, "Raj": 6, "Noemi": 1,
+          #    "Sakira": 3, "Chidi": 5}
+
+#print(check_guests(guest_list, "Adam")) # Should print 3
+#print(check_guests(guest_list, "Sakira")) # Should print 3
+#print(check_guests(guest_list, "Charley"))
